9ec026dc9c7dfe7bc2f3177dc6fb247ca804c7fa
AUTBLT409 <AUTBLT409@LT409-46.allianceblr.com>
2024-08-22 13:21:33 +0530

    core java program for Si and CI

---
+++ src/corejava/java1.java
@@ -0,0 +1,40 @@
+package corejava;
+
+import java.util.Scanner;
+
+public class java1 {
+	
+double p, t, r ;
+double si, ci;
+Scanner s;
+
+void input()
+{
+	s = new Scanner(System.in);
+	System.out.println("Enter principle amount");
+	p = s.nextDouble();
+	System.out.println("Enter time ");
+	t = s.nextDouble();
+	System.out.println("Enter rate ");
+	r = s.nextDouble();
+	
+}
+	void compute()
+	{
+		si = (p*t*r)/100;
+		ci = p*Math.pow(1+(r/100), t);
+	}
+	void display()
+	{
+		System.out.println("Simple interest is : " +si);
+		System.out.println("Compound interest is:" +ci);
+	}
+	public static void main(String[] args) {
+		java1  j = new java1();
+		j.input();
+		j.compute();
+		j.display();
+
+	}
+
+}
